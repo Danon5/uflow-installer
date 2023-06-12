80a368e88fd93b8d4edef627e118f98ce5646de6
Danon5 <danonfive@gmail.com>
2023-06-12 12:25:15 -0700

    - Imported Unitask - Renamed all namespaces and assemblies

--- UFlowInstaller.cs
+++ UFlowInstaller.cs
@@ -3,14 +3,14 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Windows;
 
-namespace DanonUnityFramework.Installer {
-    internal static class DFInstaller {
-        private const string c_url = "https://github.com/Danon5/DanonUnityFramework.git";
-        private const string c_installer_path = "Assets/DanonUnityFrameworkInstaller";
-        private static readonly string s_path = $"{Application.dataPath}/Framework/DanonUnityFramework";
+namespace UFlow.Installer {
+    internal static class UFlowInstaller {
+        private const string c_url = "https://github.com/Danon5/uflow.git";
+        private const string c_installer_path = "Assets/UFlowInstaller";
+        private static readonly string s_path = $"{Application.dataPath}/UFlow";
         private static readonly string s_cmd = $"clone --recurse-submodules -j8 {c_url} {s_path}";
 
-        [MenuItem("Assets/Install DanonUnityFramework", false, 0)]
+        [MenuItem("Assets/Install UFlow", false, 0)]
         private static void Install() {
             var startInfo = new ProcessStartInfo("git", s_cmd)
             {
